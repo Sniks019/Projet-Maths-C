df9555b4d87aa9d711a3bfee3d0d2b2d5a81bd56
Sniks019 <aliouahab451@gmail.com>
2026-04-21 09:53:13 +0200

    Add files via upload

---
+++ aff.h
@@ -0,0 +1,17 @@
+/**
+    *Affiche le menu principal.
+*/
+
+void menu();
+
+/**
+    *Affiche le menu des exercices sur les mutiplications.
+*/
+
+void aff_mult();
+
+/**
+    *Affiche le menu des exercices sur les additions.
+*/
+
+void aff_add();
---
+++ exos.h
@@ -0,0 +1,55 @@
+/**
+	*
+	*
+*/
+
+int intervertir(int *pa, int *pb);
+
+/**
+    *Deux entiers entre 1 et 10 sont choisis aleatoirement.
+    *L'utilisateur entre un entier dans guess.
+    *Si guess vaut le produit des deux entiers alors l'utilisateur a bon.
+    *L'utilisateur a 3 essais max pour trouver le resultat.
+*/
+
+int mult();
+
+/**
+	*Deux entiers sont choisis aleatoirement entre 1 et 100.
+	*L'utilisateur n'a qu'un essai.
+	*Affiche "Bravo" si vrai et "Faux" si faux.
+*/
+
+int add();
+
+/**
+	*L'utilisateur choisi d'afficher la table de multiplication d'un entier entre 1 et 10.
+	*Lorsqu'il est pret, il peut lancer l'exercice.
+	*Il doit donner les resultats de la table dans l'ordre croissant.
+	*Si toutes les reponses sont bonne --> affiche "Bravo".
+	*Si plus de la moitie des reponses sont bonnes --> affiche "Pas mal".
+	*Sinon affiche "Dommage".
+*/
+
+int tabmult();
+
+/**
+	*
+	*
+*/
+
+int divis();
+
+/**
+	*
+	*
+*/
+
+int sous();
+
+/**
+	*
+	*
+*/
+
+int suite_add();
